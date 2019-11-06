00958cd40b5975171e537ef11ae34fc05294a627
Lazar Otasevic <lazar.otasevic@1und1.de>
2019-11-07 00:48:54 +0100

    works

--- Tests/sisojeTests/FontsTests.mm
+++ Tests/sisojeTests/FontsTests.mm
@@ -9,25 +9,25 @@ using namespace sisoje_tests;
 @implementation FontsTests
 
 - (void)testFonts {
-    std::map<int, std::vector<char>> brighnerses;
-    std::unordered_map<char, std::array<std::bitset<8>, 8>> charBits;
-    std::unordered_map<char, int> charBr;
+    constexpr int asciiFontWidth = 8;
+    constexpr auto asciiFontHeight = 13;
+    typedef uint32_t pixel_sum_type;
+    std::array<std::bitset<asciiFontWidth>, asciiFontHeight> charBits[256];
     for(int c=32; c<127; ++c) {
-        const auto bitLines = sisoje::font_height_8::bit_lines(sisoje::pDefaultFont, c);
-        charBits[c] = bitLines;
-        int bright = 0;
-        for(const auto &bitLine: bitLines) {
-            bright += bitLine.count();
-            auto bits = bitLine.to_string();
-            std::reverse(bits.begin(), bits.end());
-            //std::cout << bits << std::endl;
+        charBits[c] = sisoje::font_height<asciiFontHeight>::bit_lines(sisoje::defaultFont, c-32);
+        if (c == 'j') {
+            const auto &bitLines = charBits[c];
+            for(const auto &bitLine: bitLines) {
+                for(int i=0;i<8;++i) {
+                    const auto bit = bitLine[i];
+                    char ch = bit ? 'X' : '.';
+                    std::cout << ch;
+                }
+                std::cout << std::endl;
+            }
+            std::cout << std::endl;
         }
-        brighnerses[bright].push_back(c);
-        charBr[c] = bright;
-        //std::cout << std::endl;
     }
-
-    const float maxBrightness = brighnerses.rbegin()->first;
 
     NSString *base64 = [NSString stringWithUTF8String:lena512.c_str()];
     NSData *data = [[NSData alloc] initWithBase64EncodedString:base64 options:0];
@@ -35,24 +35,16 @@ using namespace sisoje_tests;
     const uint8_t *pBytes = (uint8_t*)data.bytes;
     const int width = 512;
     const int height = 512;
-    const int asciiFontWidth = 8;
-    const int asciiFontHeight = 8;
-    const int asciiFontPixels = asciiFontWidth * asciiFontHeight;
-    const int maxPixelSumPerAscii = asciiFontPixels * 255 * 3;
-    const int numberOfPixels = width * height;
-
-    typedef uint32_t pixel_sum_type;
-
     const int asciiArtWidth = width / asciiFontWidth;
     const int asciiArtHeight = height / asciiFontHeight;
 
     char asciiArt[asciiArtHeight][asciiArtWidth];
 
-    std::unordered_map<char, pixel_sum_type> brightMatch;
+    pixel_sum_type brightMatch[256];
     for(auto ay=0; ay<asciiArtHeight; ++ay) {
         for(auto ax=0; ax<asciiArtWidth; ++ax) {
-            for(auto it=brightMatch.begin(); it != brightMatch.end(); ++it) {
-                it->second = 0;
+            for(int c=32; c<127; ++c) {
+                brightMatch[c] = 0;
             }
             for(auto y=0; y<asciiFontHeight; ++y) {
                 for(auto x=0; x<asciiFontWidth; ++x) {
@@ -63,16 +55,13 @@ using namespace sisoje_tests;
                     grayLevel += pPixelBytes[1];
                     grayLevel += pPixelBytes[2];
                     for(int c=32; c<127; ++c) {
-                        const auto bit = charBits[c].at(y)[x];
-                        auto dif = bit ? (3*255 - grayLevel) : grayLevel;
-                        brightMatch[c] += dif;
+                        const auto bit = charBits[c][y][x];
+                        brightMatch[c] += bit ? (3*255 - grayLevel) : grayLevel;
                     }
                 }
             }
-            const auto minIt = std::min_element(brightMatch.begin(), brightMatch.end(), [](auto x, auto y) {
-                return x.second < y.second;
-            });
-            const auto myc = minIt->first;
+            const auto minIt = std::min_element(brightMatch+32, brightMatch+127);
+            const char myc = minIt - brightMatch;
             asciiArt[ay][ax] = myc;
             std::cout << myc;
         }
